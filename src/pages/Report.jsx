6f8bf07b5a3cb9f38c90af022b6d3f00b8192f38
Yogeshwaran B <yogiwar33@gmail.com>
2025-06-03 12:09:14 +0530

    update the Report pages as responsive

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,57 +2,60 @@ import React from 'react'
 
 const Report = () => {
   return (
-    <div>
-            <h1 className='mb-5 ms-3' >Reports</h1>
-            <div className='card shadow-sm mb-4 gap-3'>
-                
-                <div className='d-flex gap-3'>
-                    <i class="bi bi-people-fill fs-2"> Candidate Report</i>
-                    {/* <h3>Candidate Report</h3> */}
-                </div>
-                <div className='d-flex gap-3'>
-                    <div className=' d-flex-col w-10 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
-                        {/* <img src="" alt="" /> */}
-                        <h5>PipeLine Progress report</h5>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum </p>
-                    </div>
-                    <div className=' d-flex-col w-10 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
-                        {/* <img src="" alt="" /> */}
-                        <h5>Quality report</h5>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum </p>
-                    </div>
-                    <div className=' d-flex-col w-10 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
-                        {/* <img src="" alt="" /> */}
-                        <h5>Diversity Candidate report</h5>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum </p>
-                    </div>
-                </div>
+    <div className="container-fluid px-3 px-md-4">
+      <h1 className='mb-4 mb-md-5'>Reports</h1>
+      
+      <div className='card shadow-sm mb-4 p-3 p-md-4'>
+        <div className='d-flex gap-2 mb-3'>
+          <i className="bi bi-people-fill fs-2"> Candidate Report</i>
+        </div>
+        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3'>
+          <div className='col'>
+            <div className='h-100 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
+              <h5>PipeLine Progress report</h5>
+              <p className='mb-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum</p>
             </div>
-
-        {/* Second section of the report pages */}
-
-            <div className='card shadow-sm mb-4 gap-3'>
-                <div className='d-flex gap-3 align-items-end'>
-                    <i class="bi bi-person-check fs-3 mb-0"> Employee Report </i>
-                </div>
-                <div className='d-flex gap-3'>
-                    <div className=' d-flex-col w-10 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
-                        {/* <img src="" alt="" /> */}
-                        <h5>Performance report</h5>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum </p>
-                    </div>
-                    <div className=' d-flex-col w-10 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
-                        {/* <img src="" alt="" /> */}
-                        <h5>Attendance report</h5>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum </p>
-                    </div>
-                    <div className=' d-flex-col w-10 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
-                        {/* <img src="" alt="" /> */}
-                        <h5>Engagement report</h5>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum </p>
-                    </div>
-                </div>
+          </div>
+          <div className='col'>
+            <div className='h-100 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
+              <h5>Quality report</h5>
+              <p className='mb-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum</p>
             </div>
+          </div>
+          <div className='col'>
+            <div className='h-100 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
+              <h5>Diversity Candidate report</h5>
+              <p className='mb-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='card shadow-sm mb-4 p-3 p-md-4'>
+        <div className='d-flex gap-2 mb-3'>
+          <i className="bi bi-person-check fs-3 mb-0"> Employee Report</i>
+        </div>
+        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3'>
+          <div className='col'>
+            <div className='h-100 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
+              <h5>Performance report</h5>
+              <p className='mb-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum</p>
+            </div>
+          </div>
+          <div className='col'>
+            <div className='h-100 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
+              <h5>Attendance report</h5>
+              <p className='mb-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum</p>
+            </div>
+          </div>
+          <div className='col'>
+            <div className='h-100 p-3 rounded-3' style={{backgroundColor:"#eae9ea", cursor:'pointer'}}>
+              <h5>Engagement report</h5>
+              <p className='mb-0'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quos eaque voluptatum, porro, optio nostrum</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
